1f920bab54c30abe0712e9b322394a8ec9d98ad7
Rolando Garza <rolandog@gmail.com>
2022-02-09 09:44:48 +0100

    Added references and a quote from python docs

--- python/.config/python/python_startup.py
+++ python/.config/python/python_startup.py
@@ -4,6 +4,14 @@ import readline
 
 """
 Sets ~/.python_history to $XDG_STATE_HOME/python/history
+
+The file is executed in the same namespace where interactive commands
+are executed so that objects defined or imported in it can be used
+without qualification in the interactive session.
+
+References
+----------
+[1] https://docs.python.org/3/using/cmdline.html#envvar-PYTHONSTARTUP
 """
 
 histfile = os.path.join(
