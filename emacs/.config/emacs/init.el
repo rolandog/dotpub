371eae74e95361abbd9d301103d5f1e26b18939c
Rolando Garza <rolandog@gmail.com>
2023-04-28 11:22:05 +0200

    added org-contrib so we can use ignore-headlines

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -541,10 +541,11 @@
 
 ;;; ox-extras
 ;; require the package
-;(require 'ox-extras)
+(require 'org-contrib)
+(require 'ox-extra)
 
 ;; ignore headings
-;(ox-extras-activate '(ignore-headlines))
+(ox-extras-activate '(ignore-headlines))
 
 
 ;;; Jinja2
@@ -630,7 +631,7 @@
  '(org-modules
    '(ol-bbdb ol-bibtex ol-docview ol-doi ol-eww ol-gnus org-habit ol-info ol-irc ol-mhe ol-rmail ol-w3m))
  '(package-selected-packages
-   '(org-ai plantuml-mode lsp-mode poetry biblio citeproc gnuplot htmlize jinja2-mode mexican-holidays netherlands-holidays ob-blockdiag ob-php ob-sql-mode openwith org org-drill org-roam org-roam-bibtex string-inflection))
+   '(org-contrib org-ai plantuml-mode lsp-mode poetry biblio citeproc gnuplot htmlize jinja2-mode mexican-holidays netherlands-holidays ob-blockdiag ob-php ob-sql-mode openwith org org-drill org-roam org-roam-bibtex string-inflection))
  '(reftex-bibpath-environment-variables
    '("BIBINPUTS" "TEXBIB" "/home/rolandog/Documents/references/" "/home/rolandog/org/" "/home/rolandog/org-roam/"))
  '(reftex-default-bibliography '("~/org/references.bib")))
