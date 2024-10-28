7e4699303bc2aa5997f7f832019d269c7a5dd9d9
Rolando Garza <rolandog@gmail.com>
2024-10-28 21:08:52 +0100

    added anki-editor config settings

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -712,6 +712,53 @@
   (org-table-toggle-column-width))
 
 
+;;;; anki-editor configuration
+;; thanks Cheong Yiufung: https://yiufung.net/post/anki-org/
+
+(require 'anki-editor)
+
+;; key bindings for org-mode
+(define-key org-mode-map (kbd "<f12>") 'anki-editor-cloze-region-auto-incr)
+(define-key org-mode-map (kbd "<f11>") 'anki-editor-cloze-region-dont-incr)
+(define-key org-mode-map (kbd "<f10>") 'anki-editor-reset-cloze-number)
+(define-key org-mode-map (kbd "<f9>")  'anki-editor-push-tree)
+
+;; resets cloze number to 1 after each capture
+(add-hook 'org-capture-after-finalize-hook 'anki-editor-reset-cloze-number)
+
+;; allow anki-editor to create a new deck if it doesn't exist
+(setq anki-editor-create-decks t)
+(setq anki-editor-org-tags-as-anki-tags t)
+
+;; function definitions
+(defun anki-editor-cloze-region-auto-incr (&optional arg)
+  "Cloze region without hint and increase card number."
+  (interactive)
+  (anki-editor-cloze-region my-anki-editor-cloze-number "")
+  (setq my-anki-editor-cloze-number (1+ my-anki-editor-cloze-number))
+  (forward-sexp))
+
+(defun anki-editor-cloze-region-dont-incr (&optional arg)
+  "Cloze region without hint using the previous card number."
+  (interactive)
+  (anki-editor-cloze-region (1- my-anki-editor-cloze-number) "")
+  (forward-sexp))
+
+(defun anki-editor-reset-cloze-number (&optional arg)
+  "Reset cloze number to ARG or 1"
+  (interactive)
+  (setq my-anki-editor-cloze-number (or arg 1)))
+
+(defun anki-editor-push-tree ()
+  "Push all notes under a tree."
+  (interactive)
+  (anki-editor-push-notes '(4))
+  (anki-editor-reset-cloze-number))
+
+;; initialize my-anki-editor-cloze-number to 1
+(anki-editor-reset-cloze-number)
+
+
 ;;; removed packages
 ; modules:
 ; org-annotate-file org-checklist
