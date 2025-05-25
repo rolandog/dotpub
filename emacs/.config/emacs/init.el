da15ee0acb669226cb795bd4da7427fc18ade2c7
Rolando Garza <rolandog@gmail.com>
2025-05-25 16:27:37 +0200

    set unspecified foreground to try to prevent warning

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -977,7 +977,7 @@ The function will only proceed if Ghostscript (gs) is installed on the system."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(info-index-match ((t (:background "#FDBD33" :foreground "unspecified" :weight bold)))))
 
 (require 'auto-dark)
 (auto-dark-mode t)
